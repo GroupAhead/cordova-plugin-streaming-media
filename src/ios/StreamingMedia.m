bc9e4b9f48aca42dbf71fdf2db91e17a6575dde1
Kalyan Vishnubhatla <kalvish@gmail.com>
2017-04-04 15:46:26 -0700

    Removed unnecessary code from streaming media.

--- src/ios/StreamingMedia.m
+++ src/ios/StreamingMedia.m
@@ -6,20 +6,12 @@
 
 
 @interface StreamingMedia()
-- (void)parseOptions:(NSDictionary *) options type:(NSString *) type;
 - (void)play:(CDVInvokedUrlCommand *) command type:(NSString *) type;
-- (void)setBackgroundColor:(NSString *)color;
-- (void)setImage:(NSString*)imagePath withScaleType:(NSString*)imageScaleType;
-- (UIImage*)getImage: (NSString *)imageName;
 - (void)startPlayer:(NSString*)uri;
 @end
 
 @implementation StreamingMedia {
     NSString* callbackId;
-    BOOL shouldAutoClose;
-    UIColor *backgroundColor;
-    UIImageView *imageView;
-    BOOL initFullscreen;
     AVPlayerViewController *moviePlayer;
 }
 
@@ -27,36 +19,9 @@ NSString * const TYPE_VIDEO = @"VIDEO";
 NSString * const TYPE_AUDIO = @"AUDIO";
 NSString * const DEFAULT_IMAGE_SCALE = @"center";
 
--(void)parseOptions:(NSDictionary *)options type:(NSString *) type {
-    shouldAutoClose = false;
-    backgroundColor = [UIColor blackColor];
-    initFullscreen = true;
-    
-    if ([type isEqualToString:TYPE_AUDIO]) {
-        // bgImage
-        // bgImageScale
-        if (![options isKindOfClass:[NSNull class]] && [options objectForKey:@"bgImage"]) {
-            NSString *imageScale = DEFAULT_IMAGE_SCALE;
-            if (![options isKindOfClass:[NSNull class]] && [options objectForKey:@"bgImageScale"]) {
-                imageScale = [options objectForKey:@"bgImageScale"];
-            }
-            [self setImage:[options objectForKey:@"bgImage"] withScaleType:imageScale];
-        }
-        // bgColor
-        if (![options isKindOfClass:[NSNull class]] && [options objectForKey:@"bgColor"]) {
-            NSLog(@"Found option for bgColor");
-            [self setBackgroundColor:[options objectForKey:@"bgColor"]];
-        } else {
-            backgroundColor = [UIColor blackColor];
-        }
-    }
-}
-
 -(void)play:(CDVInvokedUrlCommand *) command type:(NSString *) type {
     callbackId = command.callbackId;
     NSString *mediaUrl  = [command.arguments objectAtIndex:0];
-    [self parseOptions:[command.arguments objectAtIndex:1] type:type];
-    
     [self startPlayer:mediaUrl];
 }
 
@@ -99,61 +64,6 @@ NSString * const DEFAULT_IMAGE_SCALE = @"center";
 
 -(void)stopAudio:(CDVInvokedUrlCommand *) command {
     [self stop:command type:[NSString stringWithString:TYPE_AUDIO]];
-}
-
--(void) setBackgroundColor:(NSString *)color {
-    if ([color hasPrefix:@"#"]) {
-        // HEX value
-        unsigned rgbValue = 0;
-        NSScanner *scanner = [NSScanner scannerWithString:color];
-        [scanner setScanLocation:1]; // bypass '#' character
-        [scanner scanHexInt:&rgbValue];
-        backgroundColor = [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0];
-    } else {
-        // Color name
-        NSString *selectorString = [[color lowercaseString] stringByAppendingString:@"Color"];
-        SEL selector = NSSelectorFromString(selectorString);
-        UIColor *colorObj = [UIColor blackColor];
-        if ([UIColor respondsToSelector:selector]) {
-            colorObj = [UIColor performSelector:selector];
-        }
-        backgroundColor = colorObj;
-    }
-}
-
--(UIImage*)getImage: (NSString *)imageName {
-    UIImage *image = nil;
-    if (imageName != (id)[NSNull null]) {
-        if ([imageName hasPrefix:@"http"]) {
-            // Web image
-            image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:imageName]]];
-        } else if ([imageName hasPrefix:@"www/"]) {
-            // Asset image
-            image = [UIImage imageNamed:imageName];
-        } else if ([imageName hasPrefix:@"file://"]) {
-            // Stored image
-            image = [UIImage imageWithData:[NSData dataWithContentsOfFile:[[NSURL URLWithString:imageName] path]]];
-        } else if ([imageName hasPrefix:@"data:"]) {
-            // base64 encoded string
-            NSURL *imageURL = [NSURL URLWithString:imageName];
-            NSData *imageData = [NSData dataWithContentsOfURL:imageURL];
-            image = [UIImage imageWithData:imageData];
-        } else {
-            // explicit path
-            image = [UIImage imageWithData:[NSData dataWithContentsOfFile:imageName]];
-        }
-    }
-    return image;
-}
-
--(void)setImage:(NSString*)imagePath withScaleType:(NSString*)imageScaleType {
-    imageView = [[UIImageView alloc] initWithFrame:self.viewController.view.bounds];
-    if (imageScaleType == nil) {
-        NSLog(@"imagescaletype was NIL");
-        imageScaleType = DEFAULT_IMAGE_SCALE;
-    }
-    
-    [imageView setImage:[self getImage:imagePath]];
 }
 
 -(void)startPlayer:(NSString*)uri {
